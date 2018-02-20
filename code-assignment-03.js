db49d5d1b1e64020c4443ef325fcf9816490bd74
ctotar1 <ctotar1@users.noreply.github.com>
2018-02-19 23:57:59 -0600

    changed

---
+++ code-assignment-03.js
@@ -0,0 +1,8 @@
+let myNumber = 69
+let myString = 'New Orleans'
+let myBoolean = true
+let myArray = ['astros', 'freds', 'juul']
+console.log('My Special Number:' + myNumber)
+console.log('Text String:' + myString)
+console.log('Boolean Statement:' + myBoolean)
+console.log('Array of words for assignment:' + myArray)
