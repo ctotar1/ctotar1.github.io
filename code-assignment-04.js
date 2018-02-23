32667b306a5f9fd71dff112c44901db39f0a443b
ctotar1 <ctotar1@users.noreply.github.com>
2018-02-22 19:12:29 -0600

    code assignment 04

---
+++ code-assignment-04.js
@@ -0,0 +1,26 @@
+let projectArray = [
+  1,
+  'https://ctotar1.github.io',
+  'Chris Website',
+  'CT',
+  true,
+  ['astros', 'freds', 'juul']
+]
+
+let projectObject = {
+  id: 1,
+  url: 'https://ctotar1.github.io',
+  title: 'Chris Website'
+  desc: 'CT',
+  thumb: true,
+  keywords: [
+    'astros',
+    'freds',
+    'juul'
+  ]
+}
+
+
+console.log(projectArray[2])
+console.log(projectObject.title)
+console.log(projectObject["Chris Website"])
