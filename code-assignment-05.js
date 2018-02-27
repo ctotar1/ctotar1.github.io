af7d17e06684c301b26c4fb06c6e156466ef8a18
ctotar1 <ctotar1@users.noreply.github.com>
2018-02-26 22:11:12 -0600

    code assignment 05

---
+++ code-assignment-05.js
@@ -0,0 +1,22 @@
+let project1 = {
+  id: 1,
+  title: 'New Orleans',
+  hasThumbnail: true
+}
+let project2 = {
+  id: 2,
+  title: 'Population',
+  hasThumbnail: true
+}
+let projects = [
+  project1,
+  project2
+]
+for (let i = 0; i < projects.lenth; i++) {
+
+}
+console.log('project1' + i + 'New Orleans' + projects[i].title)
+console.log('New Orleans' + projects[i].id + 'New Orleans.jpg')
+if (project[i].hasThumbnail === true) {
+  console.log('New Orleans' + projects[i].id + 'New Orleans.jpg')
+}
