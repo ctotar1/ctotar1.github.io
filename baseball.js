635149750b853a7e6b11e5aa2725efb676b901bc
ctotar1 <ctotar1@users.noreply.github.com>
2018-03-18 21:03:23 -0500

    js

---
+++ baseball.js
@@ -0,0 +1,7 @@
+function describeBaseball (baseball) {
+  let description = 'Astros ' + baseball + ' is good '
+  return description
+}
+let myBaseball = 'Astros'
+let description = describeBaseball(myBaseball)
+console.log(description)
